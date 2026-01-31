e11206b71cff637f9262e167925b560ad038ad2a
m4rs <marcel@marcelclaramunt.cat>
2026-01-31 12:34:25 +0100

    fixes

--- binance/src/binance/core/__init__.pyi
+++ binance/src/binance/core/__init__.pyi
@@ -2,14 +2,13 @@ from .util import timestamp, round2tick, trunc2tick
 from .exc import Error, NetworkError, UserError, ValidationError, AuthError, ApiError
 from .validation import ValidationMixin, validator, TypedDict, Timestamp
 from .http import HttpClient, HttpMixin, AuthHttpClient, AuthHttpMixin
-from .mixin import Endpoint, AuthEndpoint, Router, AuthRouter, validator, Response, BITGET_REST_URL
+from .mixin import Endpoint, AuthEndpoint, Router, AuthRouter, validator, BINANCE_REST_URL
 
 __all__ = [
-  'OrderType', 'OrderStatus', 'TimeInForce',
   'timestamp', 'round2tick', 'trunc2tick',
   'Error', 'NetworkError', 'UserError', 'ValidationError', 'AuthError', 'ApiError',
   'ValidationMixin', 'validator', 'TypedDict', 'Timestamp',
   'HttpClient', 'HttpMixin', 'AuthHttpClient', 'AuthHttpMixin',
-  'Endpoint', 'AuthEndpoint', 'Router', 'AuthRouter', 'validator', 'Response',
-  'BITGET_REST_URL',
+  'Endpoint', 'AuthEndpoint', 'Router', 'AuthRouter', 'validator',
+  'BINANCE_REST_URL',
 ]
